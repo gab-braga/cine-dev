fb8ee01d4e262ca10e3c6370bdefc9432c0c3c3f
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-10 16:01:29 -0300

    feat: create home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'prefix',
+    component: HomeComponent,
+    title: 'CineDev',
+  },
   {
     path: 'login',
     component: LoginComponent,
