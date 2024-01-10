eea64f24426d877ba2479c28ca436c98b985332b
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-10 16:40:02 -0300

    bug: update style forms login and signup

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [InputTextModule, ButtonModule, CheckboxModule],
+  imports: [InputTextModule, InputMaskModule, ButtonModule, CheckboxModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
