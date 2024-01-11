b74d2f8da1724e2ba1afbd7093aa184be09ed350
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-11 01:36:25 -0300

    feat: create dashboard and users component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { UsersComponent } from './pages/admin/users/users.component';
 
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'prefix',
+    pathMatch: 'full',
     component: HomeComponent,
     title: 'CineDev',
   },
@@ -19,5 +21,20 @@ export const routes: Routes = [
     path: 'signup',
     component: SignupComponent,
     title: 'Cadastre-se',
+  },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: 'Painel de Controle',
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+        title: 'Controle de Usuários',
+      },
+    ],
   },
 ];
