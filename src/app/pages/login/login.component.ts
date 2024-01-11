30fbcf577f92bde7d99d6561b9273ca419a28f12
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-10 23:26:31 -0300

    feat: applied validators in forms

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputTextModule, ButtonModule],
+  imports: [InputTextModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginFormSubmitted: boolean = false;
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    this.loginFormSubmitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -3,12 +3,39 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputMaskModule } from 'primeng/inputmask';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [InputTextModule, InputMaskModule, ButtonModule, CheckboxModule],
+  imports: [
+    InputTextModule,
+    InputMaskModule,
+    ButtonModule,
+    CheckboxModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupFormSubmitted: boolean = false;
+  signupForm = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(120)]],
+    cpf: [
+      '',
+      [Validators.required, Validators.minLength(14), Validators.maxLength(14)],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    this.signupFormSubmitted = true;
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    }
+  }
+}
