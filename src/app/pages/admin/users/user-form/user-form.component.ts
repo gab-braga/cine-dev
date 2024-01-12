384f561aa5bf0903f92be7405a11d4075cb0b897
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-12 15:06:07 -0300

    feat: validation in form create user

--- src/app/pages/admin/users/user-form/user-form.component.ts
+++ src/app/pages/admin/users/user-form/user-form.component.ts
@@ -17,6 +17,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './user-form.component.css',
 })
 export class UserFormComponent {
+  formUserCreateSubmitted: boolean = false;
   formUserCreate = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(120)]],
     email: ['', [Validators.required, Validators.email]],
@@ -28,9 +29,14 @@ export class UserFormComponent {
 
   constructor(private fb: FormBuilder) {}
 
-  onUpload(fiel: any): void {}
+  onUpload(files: any): void {
+    const file = files.files[0];
+    if (file) console.log(file);
+    else console.log('empty');
+  }
 
   onSubmit(): void {
+    this.formUserCreateSubmitted = true;
     if (this.formUserCreate.valid) {
       console.log(this.formUserCreate.value);
     }
