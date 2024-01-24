a2593a3ed8541ef60e4ca0810b923a8e8f562843
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-24 14:19:45 -0300

    feat: update session day

--- src/app/components/session-day/session-day.component.ts
+++ src/app/components/session-day/session-day.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'c-session-day',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './session-day.component.html',
   styleUrl: './session-day.component.css',
 })
@@ -12,20 +13,34 @@ export class SessionDayComponent implements OnInit {
   public day: Date = new Date();
 
   protected dayOfWeek: string = '';
-  protected dayOfMonth: number = 0;
-  protected month: number = 0;
+  protected dayOfMonth: string = '';
+  protected month: string = '';
+
+  constructor(private router: Router) {}
 
   public ngOnInit(): void {
     this.extractValuesOfDay();
   }
 
   private extractValuesOfDay(): void {
-    this.dayOfMonth = this.day.getDate();
-    this.month = this.day.getMonth() + 1;
+    this.dayOfMonth = String(this.day.getDate()).padStart(2, '0');
+    this.month = String(this.day.getMonth() + 1).padStart(2, '0');
     this.dayOfWeek = this.day
       .toLocaleDateString('pt-BR', {
         weekday: 'long',
       })
       .split('-')[0];
+  }
+
+  protected redirectSession(): void {
+    const params = { day: this.formatDate(this.day) };
+    this.router.navigate(['/sessions', { queryParams: params }]);
+  }
+
+  private formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 }
