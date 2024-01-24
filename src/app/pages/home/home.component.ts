0099ce6db19f0272494034ffe513e9c5139f3638
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-24 10:49:43 -0300

    feat: introduction in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, HeaderComponent, FooterComponent],
+  imports: [RouterLink, HeaderComponent, FooterComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
