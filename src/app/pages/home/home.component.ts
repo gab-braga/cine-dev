334a80dcc73e5df8083b0e8a3f2a949221406d05
f-gabriel-braga <gabrielbraga0712@gmail.com>
2024-01-25 09:47:45 -0300

    feat: carousel of films

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { ButtonModule } from 'primeng/button';
 import { SessionDayComponent } from '../../components/session-day/session-day.component';
 import { SessionCardComponent } from '../../components/session-card/session-card.component';
+import { CarouselModule } from 'primeng/carousel';
 
 const NUMBER_OF_SESSIONS_SHORTCUTS = 14;
 
@@ -18,6 +19,7 @@ const NUMBER_OF_SESSIONS_SHORTCUTS = 14;
     ButtonModule,
     SessionDayComponent,
     SessionCardComponent,
+    CarouselModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
@@ -34,4 +36,27 @@ export class HomeComponent implements OnInit {
       this.sessionsDay[i] = new Date(Date.now() + i * 86400000);
     }
   }
+
+  responsiveOptions = [];
+
+  carousel = [
+    {
+      title: 'Homem de Ferro',
+      resume:
+        'Tony Stark é um industrial bilionário e inventor brilhante que realiza testes bélicos no exterior, mas é sequestrado por terroristas que o forçam a construir uma arma devastadora. Em vez disso, ele constrói uma armadura blindada e enfrenta seus sequestradores. Quando volta aos Estados Unidos, Stark aprimora a armadura e a utiliza para combater o crime.',
+      image: '../../../assets/carousel/cover1.png',
+    },
+    {
+      title: 'Vingadores',
+      resume:
+        'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes. Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade.',
+      image: '../../../assets/carousel/cover2.png',
+    },
+    {
+      title: 'Capitão América: O Primeiro Vingador',
+      resume:
+        'Steve Rogers é um jovem que participa de experiências visando a criação do supersoldado americano. Quando os oficiais militares conseguem transformá-lo em uma arma humana, eles percebem que não podem arriscar a vida do jovem nas batalhas de guerra.',
+      image: '../../../assets/carousel/cover3.png',
+    },
+  ];
 }
