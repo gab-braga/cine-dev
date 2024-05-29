5c129b11ae6667a7ef3b489e90c1ea08d99e05c1
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2024-05-28 22:34:21 -0300

    bug: update uuid films in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -59,21 +59,21 @@ export class HomeComponent implements OnInit {
 
   protected carousel = [
     {
-      uuid: '875a92c0-77b2-4e8b-9de9-fee3ab68c329',
+      uuid: 'baf0426f-1d57-11ef-a163-6045bd3a418c',
       title: 'Homem de Ferro',
       resume:
         'Tony Stark é um industrial bilionário e inventor brilhante que realiza testes bélicos no exterior, mas é sequestrado por terroristas que o forçam a construir uma arma devastadora. Em vez disso, ele constrói uma armadura blindada e enfrenta seus sequestradores. Quando volta aos Estados Unidos, Stark aprimora a armadura e a utiliza para combater o crime.',
       image: 'assets/images/carousel/cover1.png',
     },
     {
-      uuid: '7156bbdb-bb67-45a9-8820-2e0dd5804390',
+      uuid: 'baf99666-1d57-11ef-a163-6045bd3a418c',
       title: 'Vingadores: Ultimato',
       resume:
         'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
       image: 'assets/images/carousel/cover2.png',
     },
     {
-      uuid: '1fda84d4-bf95-46dc-a712-2a861ada1b41',
+      uuid: 'aee459eb-1d5a-11ef-a163-6045bd3a418c',
       title: 'Capitão América: O Primeiro Vingador',
       resume:
         'Steve Rogers é um jovem que participa de experiências visando a criação do supersoldado americano. Quando os oficiais militares conseguem transformá-lo em uma arma humana, eles percebem que não podem arriscar a vida do jovem nas batalhas de guerra.',
